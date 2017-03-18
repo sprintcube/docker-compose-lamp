8140a0d461b33224617dc36531a57081f5fe0bbd
Narendra Vaghela <narendravaghela4389@gmail.com>
2017-03-18 13:25:55 +0530

    Add database connection test script

---
+++ www/test_db.php
@@ -0,0 +1,14 @@
+<?php
+$link = mysqli_connect("db", "docker", "docker", "docker");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
