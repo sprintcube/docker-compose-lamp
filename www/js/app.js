5bd56dd059a192f2904fc2cb837285b4fbd6fd96
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-01-11 18:43:20 +0400

    UI Bugfix 3 Prerelease

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+const MarketingHeadBannerAdaptive = () => {
+    let vw = $(this).width();
+    let margin;
+    if(vw == 1024 || vw == 1280 || vw < 1280){
+        margin = "7%";
+    }
+    else if(vw == 1280 || vw == 1366 || vw < 1366){
+        margin = "7%";
+    }
+    else if(vw == 1366 || vw == 1400 || vw < 1400){
+        margin = "6%";
+    }
+    else if(vw == 1400 || vw == 1600 || vw < 1600){
+        margin = "6%";
+    }
+    else if(vw == 1600 || vw == 1800 || vw < 1800){
+        margin = "5%";
+    }
+    else{ margin = "5%"; }
+
+    $('.marketing-info > header').css("margin-left", margin);
+    
+}
+
+$(document).ready(function () {
+    MarketingHeadBannerAdaptive();
+    $(window).resize(MarketingHeadBannerAdaptive);
+});
