799aa5506a9d266a46a4f40b38c93bee7aff1d60
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-01-15 13:32:28 +0400

    Passport UI Prerelease

--- www/js/passport.js
+++ www/js/passport.js
@@ -1,7 +1,12 @@
 const lightboxopen = (e,t) => {
     $('#lightbox').removeClass('lightbox-closed');
+    $('.lightwin-passport').removeClass('lightbox-closed');
 }
-
+const lightboxclose = (e,t) => {
+    $('#lightbox').addClass('lightbox-closed');
+    $('.lightwin-passport').addClass('lightbox-closed');
+}
 $(document).ready(function () {
     $('.add-but').click(lightboxopen);
+    $('.lightwin-passport > header #right-content').click(lightboxclose);
 });
