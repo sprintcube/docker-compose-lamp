46d527bbd0ce9df54ae1982f4bd2d3ad2c4a2b04
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-01-18 17:43:18 +0400

    UI Mini Bugfix Final

--- www/js/app.js
+++ www/js/app.js
@@ -21,8 +21,33 @@ const MarketingHeadBannerAdaptive = () => {
     $('.banner-block').css("margin-left", margin);
     
 }
+const HomePageHorizontalAdaptiveLine = () => {
+    let vw = $(this).width();
+    let margin;
+    if(vw == 1024 || vw == 1280 || vw < 1280){
+        margin = "-10%";
+    }
+    else if(vw == 1280 || vw == 1366 || vw < 1366){
+        margin = "-9%";
+    }
+    else if(vw == 1366 || vw == 1400 || vw < 1400){
+        margin = "-2%";
+    }
+    else if(vw == 1400 || vw == 1600 || vw < 1600){
+        margin = "-2%";
+    }
+    else if(vw == 1600 || vw == 1800 || vw < 1800){
+        margin = "-8%";
+    }
+    else{ margin = "-8%"; }
+
+    $('#promo > header hr').css({"position": "relative", "top": margin});
+}
 
 $(document).ready(function () {
     MarketingHeadBannerAdaptive();
+    HomePageHorizontalAdaptiveLine();
+
     $(window).resize(MarketingHeadBannerAdaptive);
+    $(window).resize(HomePageHorizontalAdaptiveLine);
 });
