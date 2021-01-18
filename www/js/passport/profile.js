b6f081e86cca6c7d32ac4afcb69145a58715a01e
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-01-18 19:09:53 +0400

    Profile UI Page Release Beta 1

--- www/js/passport/profile.js
+++ www/js/passport/profile.js
@@ -1,3 +1,21 @@
+const inputSwitcher = (e,t) => {
+    let index = $(".passport-page > main .profile-settings .editor input").index(this);
+    let cuel = index + 1;
+    let form = [
+        [$('.passport-page > main .profile-settings .editor:nth-child(' + cuel + ') input'), $('.passport-page > main .profile-settings .editor:nth-child(' + cuel + ') #control')],
+        ['<i class="far fa-edit"></i>', '<i class="far fa-save inputing"></i>']
+    ];
+
+
+    if(form[0][0].attr('disabled')){
+        form[1][0].html(form[1][1]);
+        form[0][0].removeAttr('disabled');
+    }
+    else{
+        form[1][0].html(form[0][1]);
+        form[0][0].attr('disabled');
+    }
+}
 $(document).ready(function () {
-    
+    $('.passport-page > main .profile-settings .editor #control').click(inputSwitcher);
 });
