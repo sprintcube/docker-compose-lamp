9ff7fa32d3ebea57f0d06828e7bafe5d98800b76
Sergey Zolotaryow <zolotaryow.mswebdev@outlook.com>
2021-08-27 17:19:16 +0400

    Work Migration

---
+++ www/investportal/basic/views/site/passport/page.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+$this->title = "Passport";
+?>
+
+<main class="main" style="background-color:   #eff3f4;">
+	<section class="section" id="link-switcher">
+            <a href="">Main</a> <span id="delimeter"> / </span> <a href="" class="active">Passport</a>
+	</section>
+	<section class="section" id="passport"></section>
+</main>
---
+++ www/investportal/basic/web/js/react/passport.js
@@ -0,0 +1,45 @@
+class PassportService extends React.Component{
+	componentDidMount(){
+		
+	}
+	render(){
+		
+	}
+}
+
+class AccountService extends React.Component{
+	componentDidMount(){
+		
+	}
+	render(){
+		
+	}
+}
+
+class OffersService extends React.Component{
+	componentDidMount(){
+		
+	}
+	render(){
+		
+	}
+}
+
+class CartService extends React.Component{
+	componentDidMount(){
+		
+	}
+	render(){
+		
+	}
+}
+
+class UserEventsService extends React.Component{
+	componentDidMount(){
+		
+	}
+	render(){
+		
+	}
+}
+
