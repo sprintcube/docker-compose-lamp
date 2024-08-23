46638b3b17bc716aa8522ef674a1c5a58fae497c
SubaruSama <andresfauque@gmail.com>
2024-08-22 22:26:58 -0300

    Update index.php
    Since i am running the docker inside a VM, using localhost is not feasible.
    I could create a port redirect and make some network wizardry, but it would get messy. And using this manner, it uses the variables defined by the dev.

--- www/index.php
+++ www/index.php
@@ -54,7 +54,7 @@
                         <div class="content">
                             <ul>
                                 <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
+                                <li><a href="http://<?=$_SERVER['SERVER_NAME']?:<?=$_ENV['PMA_PORT']?>phpMyAdmin</a></li>
                                 <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
                                 <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
                             </ul>
