1870b2fa207a2acdf5434cbb6c8f94d7c8107e85
Natalia Zhavoronok <zhvrnk97@gmail.com>
2025-04-28 08:26:47 +0300

    Draft the workflow for admins

--- www/page-parts/list-item-device-booking.php
+++ www/page-parts/list-item-device-booking.php
@@ -3,6 +3,7 @@ require_once './utils.php';
 $loan_start = date_format(date_create($row['loan_start']), 'D, d M Y');
 $loan_end = date_format(date_create($row['loan_end']), 'D, d M Y');
 $device_sn = $row['device_sn'];
+$teacher_id = $row['teacher_id'];
 $DUMMY_IMG_SRC = 'https://dummyimage.com/600x350/ced4da/6c757d';
 $image_path = "assets/images/devices/" . $device_sn . '.png';
 $img_src = file_exists($image_path) ? $image_path : $DUMMY_IMG_SRC;
@@ -21,6 +22,12 @@ $img_src = file_exists($image_path) ? $image_path : $DUMMY_IMG_SRC;
           <small class="text-muted"><?php echo $loan_start; ?> &mdash; <?php echo $loan_end; ?></small></p>
           <?php if (is_allowed_user_role([ROLE_ADMIN, ROLE_SUPER_ADMIN])) { ?>
             <small><strong>Booked by:</strong> <?php echo $row['user_fullname']; ?></small>
+              <form method="POST" action="loandevice.php?sn=<?php echo $device_sn; ?>">
+                  <input hidden type="text" name="teacher_id" value="<?php echo $teacher_id; ?>" />
+                  <input hidden type="date" name="loan_start" value="<?php echo $row['loan_start'];?>" />
+                  <input hidden type="date" name="loan_end" value="<?php echo $row['loan_end'];?>" />
+                  <button type="submit" class="btn btn-primary">Loan device</button>
+              </form>
           <?php } ?>
         </div>
       </div>
