8df3cb3f63a2478e7049f833be0e07f523a6abc7
Natalia Zhavoronok <zhvrnk97@gmail.com>
2025-05-13 00:06:54 +0300

    Refactor DB migrations + create tables for booking notifications and returns

--- config/initdb/01-laite.sql
+++ config/initdb/01-laite.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS laite (
     sn VARCHAR(32) NOT NULL,
     name VARCHAR(128),
     category VARCHAR(64),
+    description VARCHAR(1000),
     PRIMARY KEY (sn)
 );
--- config/initdb/03-users.sql
+++ config/initdb/03-users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INT(11) NOT NULL AUTO_INCREMENT,
-    username VARCHAR(30) NOT NULL,
+    username VARCHAR(7) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role ENUM('superadmin', 'admin', 'user') NOT NULL DEFAULT 'user',
     email VARCHAR(127) NOT NULL,
