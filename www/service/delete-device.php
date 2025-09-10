421b8cbba17e7bc703d944fb9699ba2e7132bb2f
Natalia Zhavoronok <zhvrnk97@gmail.com>
2025-09-10 22:37:53 +0300

    fixed delete page error

--- www/service/delete-device.php
+++ www/service/delete-device.php
@@ -4,6 +4,9 @@ require_once '../utils.php';
 
 require_once '../db/devices-functions.php';
 
+$EXCEPTION_DELETE_UNAVAILABLE_ERROR = "Cannot delete or update a parent row: a foreign key constraint fails";
+$is_device_delete_success = false;
+
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die("Connection failed");
 session_start();
@@ -13,7 +16,18 @@ if (!is_allowed_user_role([ROLE_ADMIN, ROLE_SUPER_ADMIN])) {
     exit;
 };
 
-if (isset($_POST['device_sn']) && delete_device($conn, $_POST['device_sn'])) {
+try {
+    $is_device_delete_success = delete_device($conn, $_POST['device_sn']);
+} catch (Exception $e) {
+    if (strstr($e, $EXCEPTION_DELETE_UNAVAILABLE_ERROR)) {
+        header("Location: /errors/delete-error.html");
+    } else {
+        echo $e->getMessage();
+        header("Location: /errors/error.html");
+    }
+}
+
+if (isset($_POST['device_sn']) && $is_device_delete_success) {
     header('Location: ../index.php');
     exit;
 }
