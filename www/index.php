9de67fd6b49b34a0b9110018ef9b42ff3beb8237
Natalia Zhavoronok <zhvrnk97@gmail.com>
2025-09-14 23:09:10 +0300

    change footer

--- www/index.php
+++ www/index.php
@@ -168,21 +168,7 @@ if (is_logged_in() && is_allowed_user_role([ROLE_USER])) {
         <?php } ?>
     </main>
     <!-- Footer-->
-    <footer class="bg-dark py-4 mt-auto">
-        <div class="container px-5">
-            <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                <div class="col-auto">
-                    <div class="small m-0 text-white">Copyright &copy; Your Website 2023</div>
-                </div>
-                <div class="col-auto">
-                    <a class="link-light small" href="#!">Privacy</a>
-                    <span class="text-white mx-1">&middot;</span>
-                    <a class="link-light small" href="#!">Terms</a>
-                    <span class="text-white mx-1">&middot;</span>
-                    <a class="link-light small" href="#!">Contact</a>
-                </div>
-            </div>
-        </div>
+    <footer class="bg-dark py-5 mt-auto">
     </footer>
     <?php
     if (is_allowed_user_role([ROLE_ADMIN, ROLE_SUPER_ADMIN]))
